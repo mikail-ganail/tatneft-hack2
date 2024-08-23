594918099931981d5e28f6c7f46b4a2691401f7e
Gosman Ganeev <usmangan43@gmail.com>
2024-08-23 18:34:51 +0300

    bot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BotBoxComponent } from './bot/bot-box/bot-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bot/bot-box/bot-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bot-box',
+  templateUrl: './bot-box.component.html',
+  styleUrls: ['./bot-box.component.scss']
+})
+export class BotBoxComponent {
+
+}
