21debf76f93c4cfd949a2b5c1b8b39f097cc5df5
Insafch1k <ainsaf833@gmail.com>
2024-08-23 19:03:24 +0300

    routing prettirrc home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BotBoxComponent } from './bot/bot-box/bot-box.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'bot',
+		component: BotBoxComponent,
+		title: 'ботинок',
+	},
+
+	{
+		path: 'home',
+		component: HomeComponent,
+		title: 'home',
+	},
+	{
+		path: '**',
+		component: HomeComponent,
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
